e78549213cf809b0eed00b7ae8fcca54c718efc7
wellotheman <114491798+wellotheman@users.noreply.github.com>
2022-09-27 17:38:10 +0500

    Add files via upload

---
+++ 268Fixer.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Please not this may not work for all devices.
+echo ----------CREDITS----------
+echo Atlas X Invite: https://dsc.gg/atlasx
+echo ----------CREDITS----------
+pause
+cls
+echo Getting ready.
+echo Please type "Y" and hit enter if a confirmation prompt appears.
+echo Loading.
+echo Authorizing.
+echo Confirming.
+del C:\Temp
+echo Files that are being used will stay!
+echo Perparing to exit.
+echo ...
+pause
+cls
+exit
